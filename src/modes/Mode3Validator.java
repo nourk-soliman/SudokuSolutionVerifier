57012b66a56acc995597c1c89400e6e97f562dfd
nourk-soliman <es.nour.soliman2023@alexu.edu.eg>
2025-11-28 21:15:01 +0200

    Add files via upload

---
+++ src/modes/Mode3Validator.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modes;
+
+/**
+ *
+ * @author Nour
+ */
+public class Mode3Validator {
+    
+}
